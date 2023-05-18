b9505a9a2ec9dcd5ce499df612e43ac6cb4a7aa1
klthemathnerd8 <91514631+klthemathnerd8@users.noreply.github.com>
2023-05-17 22:33:06 -0700

    Create script.js

---
+++ html/script.js
@@ -0,0 +1,51 @@
+var socket;
+var usernameInput
+var chatIDInput;
+var messageInput;
+var chatRoom;
+var messages = [];
+var delay = true;
+
+function onload(){
+  socket = io();
+  usernameInput = document.getElementById("NameInput");
+  chatIDInput = document.getElementById("IDInput");
+  messageInput = document.getElementById("ComposedMessage");
+  chatRoom = document.getElementById("RoomID");
+
+  socket.on("join", function(room){
+    chatRoom.innerHTML = "Chatroom : " + room;
+  })
+
+  socket.on("recieve", function(message){
+    console.log(message);
+    if (messages.length < 9){
+      messages.push(message);
+    }
+    else{
+      messages.shift();
+      messages.push(message);
+    }
+    for (i = 0; i < messages.length; i++){
+        document.getElementById("Message"+i).innerHTML = messages[i];
+        document.getElementById("Message"+i).style.color = "#303030";
+    }
+  })
+}
+
+function Connect(){
+  socket.emit("join", chatIDInput.value, usernameInput.value);
+}
+
+function Send(){
+  if (delay && messageInput.value.replace(/\s/g, "") != ""){
+    delay = false;
+    setTimeout(delayReset, 1000);
+    socket.emit("send", messageInput.value);
+    messageInput.value = "";
+  }
+}
+
+function delayReset(){
+  delay = true;
+}
